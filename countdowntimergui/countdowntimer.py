4e0d0df50337cf2de3b795cac595f9a54c6a648e
Michael Keene <mike@Michaels-MacBook-Pro.local>
2024-02-28 14:43:19 -0700

    first commit for countdowntimergui

---
+++ countdowntimergui/countdowntimer.py
@@ -0,0 +1,16 @@
+# countdown timer gui
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Here's my timer")
+greeting.pack()
+
+# this listens for event loops, like button clicks or keypresses
+# it blocks any code after it from running until you close the window where you called the method
+# window.mainloop()
+
+# output to console
+print('testing')
+
+
